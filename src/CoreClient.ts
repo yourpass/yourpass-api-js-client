667f7cbc735a672c206d52d6acf22dc36d781d9a
Michal Cerny <>
2018-11-22 15:09:02 +0100

    add some tsdoc

--- src/CoreClient.ts
+++ src/CoreClient.ts
@@ -2,17 +2,26 @@ import { UUID, Pass, PassBase, List, ImageReferenceMap, Query } from "./models";
 import DefaultClient from "./DefaultClient";
 import { API_URL } from "./constants/enviroments";
 
+/**
+ * A class representing a core client. It prvides interface to work with Passes, Images and templates.
+ */
 export default class CoreClient extends DefaultClient {
-
-
   public passList(query?: Query): Promise<List<PassBase>> {
     return this.list<PassBase>(API_URL.CORE, "pass", query);
   }
 
+  /**
+   * Read pass instance with ID
+   * @param id pass id
+   */
   public passGet(id: UUID): Promise<Pass> {
     return this.get<Pass>(API_URL.CORE, "pass", id);
   }
 
+  /**
+   * Create new pass
+   * @param pass new pass object
+   */
   public passCreate(pass: {
     templateId: UUID;
     dynamicData?: any;
@@ -21,6 +30,11 @@ export default class CoreClient extends DefaultClient {
     return this.create<Pass>(API_URL.CORE, "pass", pass);
   }
 
+  /**
+   * Patch pass 
+   * @param id
+   * @param pass
+   */
   public passPatch(
     id: UUID,
     pass: {
@@ -33,7 +47,10 @@ export default class CoreClient extends DefaultClient {
     return this.patch<Pass>(API_URL.CORE, "pass", id, pass);
   }
 
-  /*
+  /**
+   * Update pass
+   * @param pass 
+   */
   public passUpdate(pass: {
     id: UUID;
     templateId: UUID;
@@ -41,14 +58,18 @@ export default class CoreClient extends DefaultClient {
     dynamicImages?: ImageReferenceMap;
     voided?: boolean;
   }): Promise<Pass> {
-    return this.update<Pass>("pass", pass.id, {
+    return this.update<Pass>(API_URL.CORE, "pass", pass.id, {
       templateId: pass.templateId,
       dynamicData: pass.dynamicData,
       dynamicImages: pass.dynamicImages,
       voided: pass.voided,
     });
   }
-  */
+
+  /**
+   *  Delete Pass
+   * @param pass 
+   */
   public passDelete(pass: UUID): Promise<Pass> {
     return this.delete<Pass>(API_URL.CORE, "pass", pass);
   }
--- src/DefaultClient.ts
+++ src/DefaultClient.ts
@@ -8,31 +8,48 @@ import {
   ViewerOptions,
 } from "./models";
 import { Enviroment, API_URL, profiles } from "./constants/enviroments";
-function appendUrlParam(query: string, name: string, value: any): string {
-  return `${query}${query === "" ? "" : "&"}${name}=${encodeURI(
-    JSON.stringify(value),
-  )}`;
-}
+import { appendUrlParam } from "./helpers/url";
 
-export default class DefaultClient implements ClientOptions {
-  public fetch: Fetch;
+/**
+ * A class representing a basic client, which contains:
+ *  - common http fetch (request) function.
+ *  - common methods for REST resource CRUD operations (List, Get, Patch, Delete,..)
+ *  - gother common helper functions
+ */
+export default class DefaultClient {
+  /** fetchFunction function for call http request */
+  public fetchFunction: Fetch;
+  /** enviroment for which was client created */
   public enviroment: Enviroment;
 
+  /**
+   * Create a DefaultClient.
+   * @param {ClientOptions} opts - Contains fetch function and enviroment.
+   */
   constructor(opts: ClientOptions) {
-    this.fetch = opts.fetch;
+    this.fetchFunction = opts.fetch;
     this.enviroment = opts.enviroment || Enviroment.PRODUCTION;
   }
 
+  /**
+   * Returns base url for api and current enviroment
+   */
   public getURL(api: API_URL) {
     return profiles[this.enviroment][api];
   }
 
-  public request<T>(input: RequestInfo, init: RequestInit): Promise<T> {
-    return this.fetch(input, init).then((r: Response) => {
+  /**
+   * Common http request function
+   */
+  public fetch<T>(input: RequestInfo, init: RequestInit): Promise<T> {
+    return this.fetchFunction(input, init).then((r: Response) => {
       return r.json();
     });
   }
 
+  /**
+   * List of resource on selected api for specified query
+   */
   public list<T>(
     api: API_URL,
     resource: string,
@@ -48,54 +65,72 @@ export default class DefaultClient implements ClientOptions {
       queryStr = `?${queryStr}`;
     }
 
-    return this.request(`${this.getURL(api)}/v1/${resource}${queryStr}`, {
+    return this.fetch(`${this.getURL(api)}/v1/${resource}${queryStr}`, {
       method: "GET",
     });
   }
 
+  /**
+   * Get instance of resource on selected api for specified id
+   */
   public get<T>(api: API_URL, resource: string, id: UUID): Promise<T> {
-    return this.request(`${this.getURL(api)}/v1/${resource}/${id}`, {
+    return this.fetch(`${this.getURL(api)}/v1/${resource}/${id}`, {
       method: "GET",
     });
   }
 
+  /**
+   * Create new instance of resource on selected api
+   */
   public create<T>(api: API_URL, resource: string, object: any): Promise<T> {
-    return this.request(`${this.getURL(api)}/v1/${resource}`, {
+    return this.fetch(`${this.getURL(api)}/v1/${resource}`, {
       method: "POST",
       body: JSON.stringify(object),
     });
   }
 
+  /**
+   * Update instance with id of resource on selected api
+   */
   public update<T>(
     api: API_URL,
     resource: string,
     id: UUID,
     object: any,
   ): Promise<T> {
-    return this.request(`${this.getURL(api)}/v1/${resource}/${id}`, {
+    return this.fetch(`${this.getURL(api)}/v1/${resource}/${id}`, {
       method: "PUT",
       body: JSON.stringify(object),
     });
   }
 
+  /**
+   * Patch instance with id of resource on selected api
+   */
   public patch<T>(
     api: API_URL,
     resource: string,
     id: UUID,
     object: any,
   ): Promise<T> {
-    return this.request(`${this.getURL(api)}/v1/${resource}/${id}`, {
+    return this.fetch(`${this.getURL(api)}/v1/${resource}/${id}`, {
       method: "PATCH",
       body: JSON.stringify(object),
     });
   }
 
+  /**
+   * Delete instance with id of resource on selected api
+   */
   public delete<T>(api: API_URL, resource: string, id: UUID): Promise<T> {
-    return this.request(`${this.getURL(api)}/v1/${resource}/${id}`, {
+    return this.fetch(`${this.getURL(api)}/v1/${resource}/${id}`, {
       method: "DELETE",
     });
   }
 
+  /**
+   * Returns current user object (viewer) instance
+   */
   public getViewer(): Promise<Viewer> {
     return this.get<ViewerOptions>(API_URL.CORE, "user", "me").then(
       (resp: ViewerOptions) => {
