00873a91f4f62fca2712ab493875e3a344429eb1
Michal Cerny <>
2018-11-25 10:20:31 +0100

    documentation is in markdown

--- docs/assets/js/search.js
+++
@@ -1,3 +0,0 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"1":"External module","65536":"Type literal"},"rows":[{"id":0,"kind":65536,"name":"__type","url":"modules/_src_fetch_fetch_.html#fetch.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported","parent":"\"src/fetch/Fetch\".Fetch"},{"id":1,"kind":1,"name":"\"index\"","url":"modules/_index_.html","classes":"tsd-kind-external-module"}]};
