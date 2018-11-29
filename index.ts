c41a8accc7b7ffb5f7cf70a43c25de9919e860eb
Michal Cerny <>
2018-11-29 11:25:13 +0100

    added http Error

--- index.ts
+++ index.ts
@@ -2,5 +2,7 @@ import DefaultClient from "./src/DefaultClient";
 import CoreClient from "./src/CoreClient";
 export * from "./src/models/index";
 export * from "./src/fetch/index";
-export * from "./src/constants/enviroments"
+export * from "./src/constants/enviroments";
+export * from "./src/helpers/httpError";
 export default CoreClient;
+export { DefaultClient, CoreClient };
